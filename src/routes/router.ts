b33a01d0bc4fba911f482645419e0f814f61a9a1
thispranjalsharma <pranjalsharmaps5@gmail.com>
2025-12-15 13:42:03 +0530

    Update index.js

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v1 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from Docker v2 🎉" }));
 
 router.get("/health", (req, res) => {
   // throw new Error("Internal Server Error");
